958420689791c5db09de2c7fc34db1d41c33af87
Viktor Perdomo <vperdomo@orangetheoryfitness.com>
2017-12-14 16:33:04 -0500

    Note Files from local test

---
+++ notes-app-client/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import Routes from "./Routes";
+import { Link } from "react-router-dom";
+import { Nav, Navbar } from "react-bootstrap";
+import RouteNavItem from "./components/RouteNavItem";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App container">
+        <Navbar fluid collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to="/">Scratch</Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <RouteNavItem href="/signup">Signup</RouteNavItem>
+              <RouteNavItem href="/login">Login</RouteNavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Routes />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ notes-app-client/src/config.js
@@ -0,0 +1,6 @@
+export default {
+  cognito: {
+    USER_POOL_ID: "us-east-1_WLM5nYbwZ",
+    APP_CLIENT_ID: "12jc1lv3c4spgqk5sjgvpg0n2"
+  }
+};
